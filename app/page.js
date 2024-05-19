c3bebf89ad3fa0cf93e5ceb3a3a7d8c18502bf57
gmsk8011 <gmsk8011@gmail.com>
2024-05-19 22:41:39 +0530

    Mobile view fixed

--- app/page.js
+++ app/page.js
@@ -13,12 +13,12 @@ const page = () => {
   return (
     <>
       <Nav />
-      <div className="bg-white">
+      <div className="bg-white mt-16">
         <Image src={img} width="100%" alt="board" />
       </div>
-      <div className="flex">
-        <div className="flex flex-col p-10 gap-7 w-1/2">
-          <div className="text-gray-900 font-bold text-4xl">
+      <div className="flex max-sm:flex max-sm:flex-col">
+        <div className="flex flex-col p-10 gap-7 w-1/2 max-sm:w-full">
+          <div className="text-gray-900 font-bold text-4xl max-sm:text-3xl">
             What is BrainSproutGames?
           </div>
           <div className="text-gray-900 font-semibold text-xl">
@@ -32,70 +32,70 @@ const page = () => {
             neque aspernatur!
           </div>
         </div>
-        <div className="w-1/2 p-10 flex justify-center">
+        <div className="w-1/2 p-10 flex justify-center max-sm:w-full">
           <Image className="rounded-2xl" src={img2} width={400} alt="game" />
         </div>
       </div>
-      <div className="bg-gray-900 -skew-y-1 w-full p-10 mb-20">
-        <div className="text-center text-white text-4xl skew-y-1">
+      <div className="bg-gray-900 -skew-y-2 w-full p-10 mb-20 max-sm:mb-10">
+        <div className="text-center text-white text-4xl skew-y-2">
           Our Board Games
         </div>
-        <section class="text-white body-font">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-wrap -m-4">
-              <div class="lg:w-1/3 md:w-1/2 p-4 w-full">
-                <a class="block relative h-48 rounded overflow-hidden">
+        <section className="text-white body-font skew-y-2">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              <div className="lg:w-1/3 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
                   <Image src={img2} alt="prodict" />
                 </a>
-                <div class="mt-4">
-                  <h3 class="text-xs tracking-widest title-font mb-1">
+                <div className="mt-4">
+                  <h3 className="text-xs tracking-widest title-font mb-1">
                     CATEGORY
                   </h3>
-                  <h2 class="title-font text-lg font-medium">The Catalyzer</h2>
-                  <p class="mt-1">$16.00</p>
+                  <h2 className="title-font text-lg font-medium">The Catalyzer</h2>
+                  <p className="mt-1">$16.00</p>
                 </div>
               </div>
-              <div class="lg:w-1/3 md:w-1/2 p-4 w-full">
-                <a class="block relative h-48 rounded overflow-hidden">
+              <div className="lg:w-1/3 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
                   <Image src={img3} alt="prodict" />
                 </a>
-                <div class="mt-4">
-                  <h3 class="text-xs tracking-widest title-font mb-1">
+                <div className="mt-4">
+                  <h3 className="text-xs tracking-widest title-font mb-1">
                     CATEGORY
                   </h3>
-                  <h2 class="title-font text-lg font-medium">Shooting Stars</h2>
-                  <p class="mt-1">$21.15</p>
+                  <h2 className="title-font text-lg font-medium">Shooting Stars</h2>
+                  <p className="mt-1">$21.15</p>
                 </div>
               </div>
-              <div class="lg:w-1/3 md:w-1/2 p-4 w-full">
-                <a class="block relative h-48 rounded overflow-hidden">
+              <div className="lg:w-1/3 md:w-1/2 p-4 w-full">
+                <a className="block relative h-48 rounded overflow-hidden">
                   <Image src={img2} alt="prodict" />
                 </a>
-                <div class="mt-4">
-                  <h3 class="text-xs tracking-widest title-font mb-1">
+                <div className="mt-4">
+                  <h3 className="text-xs tracking-widest title-font mb-1">
                     CATEGORY
                   </h3>
-                  <h2 class="title-font text-lg font-medium">Neptune</h2>
-                  <p class="mt-1">$12.00</p>
+                  <h2 className="title-font text-lg font-medium">Neptune</h2>
+                  <p className="mt-1">$12.00</p>
                 </div>
               </div>
             </div>
           </div>
         </section>
       </div>
-      <div className="p-10 text-gray-900 flex justify-center items-center">
-        <div className="w-1/2 flex items-center flex-col justify-center border-r-2 border-gray-900">
-          <div className="font-bold text-3xl">Follow Us!</div>
-          <div className="flex gap-8 py-10">
-            <Image src={ig} width={70} alt="ig"></Image>
-            <Image src={fb} width={70} alt="fb"></Image>
+      <div className="p-10 text-gray-900 flex justify-center items-center max-sm:flex max-sm:flex-col max-sm:gap-6 max-sm:p-8">
+        <div className="w-1/2 flex items-center flex-col justify-center border-r-2 border-gray-900 max-sm:border-none">
+          <div className="font-bold text-3xl max-sm:text-2xl">Follow Us!</div>
+          <div className="flex gap-8 py-10 max-sm:gap-10 max-sm:py-4">
+            <Image className="max-sm:h-14 max-sm:w-auto" src={ig} width={70} alt="ig"></Image>
+            <Image className="max-sm:h-14 max-sm:w-auto" src={fb} width={70} alt="fb"></Image>
           </div>
         </div>
-        <div className="w-1/2 flex items-center flex-col justify-center border-l-2 border-gray-900">
-          <div className="font-bold text-3xl">Contact Us!</div>
-          <div className="flex gap-8 py-10">
-            <Image src={mail} width={90} alt="mail"></Image>
-            <Image src={wp} width={70} alt="wp"></Image>
+        <div className="w-1/2 flex items-center flex-col justify-center border-l-2 border-gray-900  max-sm:border-none">
+          <div className="font-bold text-3xl max-sm:text-2xl">Contact Us!</div>
+          <div className="flex gap-8 py-10 max-sm:gap-10 max-sm:py-4">
+            <Image className="max-sm:w-auto max-sm:h-12" src={mail} width={90} alt="mail"></Image>
+            <Image className="max-sm:h-14 max-sm:w-auto" src={wp} width={70} alt="wp"></Image>
           </div>
         </div>
       </div>
--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/public/assets/logo3.png";
+import { useState } from "react";
 
 const Nav = () => {
+  const [menu, setmenu] = useState(false);
   return (
-    <nav className="fixed w-full z-10 border-gray-200 bg-gray-900">
+    <nav className="fixed w-full top-0 z-10 border-gray-200 bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div className="flex gap-4">
           <Image src={logo} width={60} alt="logo" />
@@ -19,9 +23,27 @@ const Nav = () => {
             <li>Contact Us</li>
           </ul>
         </div>
-        <div className="absolute text-white w-1/2 p-6 lg:hidden flex right-0 top-0 h-full bg-gray-900">
+        {menu ? (
+          <div></div>
+        ) : (
+          <div
+            className="text-white cursor-pointer"
+            onClick={() => setmenu(!menu)}
+          >
+            Menu
+          </div>
+        )}
+        <div
+          className={`top-0 right-0 w-1/2 bg-gray-900 text-white p-6 fixed h-full z-40 ease-in-out duration-300
+          ${menu ? "translate-x-0 " : "translate-x-full"}`}
+        >
           <ul className="flex flex-col gap-4 w-full">
-            <li className="self-end">X</li>
+            <li
+              className="self-end text-3xl cursor-pointer"
+              onClick={() => setmenu(!menu)}
+            >
+              X
+            </li>
             <li>Home</li>
             <li>Games</li>
             <li>Our Story</li>
